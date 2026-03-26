431deff9aae86c6f1b3e5010b3af99dc1326f121
CristianRedCer <credcer2212@g.educaand.es>
2026-03-26 23:42:02 +0100

    arreglando problemas todavia

--- barraprogreso.js
+++
@@ -1,9 +0,0 @@
-const pct = document.getElementById("pct");
-let val = 40;
-let dir = 1;
-setInterval(() => {
-  val += dir * 0.3;
-  if (val >= 85) dir = -1;
-  if (val <= 40) dir = 1;
-  pct.textContent = Math.round(val) + "%";
-}, 50);
